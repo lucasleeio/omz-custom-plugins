6426236e6e89133e958ee1e228266fc419756912
Lucas Lee <replicatal@gmail.com>
2022-09-15 20:14:46 +0800

    update

---
+++ git-aliases/git-aliases.plugin.zsh
@@ -0,0 +1,9 @@
+alias gbvv="git branch -vv"
+
+alias gw="git worktree"
+alias gwl="git-fzf worktree list"
+alias gwp="git-fzf worktree prune"
+alias gwa="git-fzf worktree add"
+alias gwr="git-fzf worktree remove"
+
+alias gz="git-fzf"
--- git-worktree-aliases/git-worktree-aliases.plugin.zsh
+++
@@ -1,5 +0,0 @@
-alias gwt='git worktree'
-alias gwtl='git worktree list'
-alias gwtp='git worktree prune'
-alias gwta='git worktree add'
-alias gwtr='git worktree remove'
